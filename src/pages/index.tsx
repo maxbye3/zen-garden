7b153b7ecd47a6ccaa6114a0ff77ce80fb423bc7
max bye <mbye@dare.global>
2023-04-30 13:24:43 +0100

    tiles patterns are good

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,145 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const Grid: React.FC = () => {
+  const [grid, setGrid] = useState<number[][]>(
+    Array(10).fill(Array(10).fill(0))
+  );
+  const [currentColor, setCurrentColor] = useState<number>(1);
+  const [prevX, setPrevX] = useState<number | null>(null);
+  const [prevY, setPrevY] = useState<number | null>(null);
+  const [previousDirection, setPreviousDirection] = useState("");
+  const numUnfilledSquares = grid.reduce(
+    (acc, row) => acc + row.filter((col) => col === 0).length,
+    0
+  );
 
-export default function Home() {
+  // function to generate random number between min and max (inclusive)
+  const getRandomIntInclusive = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const handleSquareClick = (x: number, y: number): void => {
+    const styleSheet = document.styleSheets[0]; // get the first stylesheet in the document
+    // onhover cover tile
+    const currentTile = `.color-${currentColor} { background-color: purple}`;
+    styleSheet.insertRule(currentTile); // insert the CSS class into the stylesheet
+
+    if (grid[x][y] !== 0) {
+      console.log(`Square (${x}, ${y}) is already filled.`);
+      return;
+    }
+    if (prevX === null || prevY === null) {
+      console.log(`Square (${x}, ${y}) is the first filled square.`);
+      const newGrid = grid.map((row, i) =>
+        row.map((col, j) => (i === x && j === y ? currentColor : col))
+      );
+      setGrid(newGrid);
+      setCurrentColor(currentColor + 1);
+      setPrevX(x);
+      setPrevY(y);
+    } else {
+      const dx = x - prevX;
+      const dy = y - prevY;
+      let direction: string;
+      let borderPos,
+        borderRadius: string | boolean = false,
+        angle = "";
+      if (dx === 1 && dy === 0) {
+        if (previousDirection === "left") {
+          borderRadius = "300px 0 0 0";
+          borderPos = "100% 100%";
+        } else if (previousDirection === "right") {
+          borderRadius = "0 300px 0 0";
+          borderPos = "0 100%";
+        } else {
+          angle = "90deg";
+        }
+        direction = "down";
+        console.log("current direction: " + direction);
+      } else if (dx === -1 && dy === 0) {
+        if (previousDirection === "left") {
+          borderRadius = "0 0 0 300px";
+          borderPos = "100% 0";
+        } else if (previousDirection === "right") {
+          borderRadius = "0 0 300px 0";
+          borderPos = "0 0";
+        } else {
+          angle = "90deg";
+        }
+        direction = "up";
+      } else if (dx === 0 && dy === 1) {
+        if (previousDirection === "up") {
+          borderRadius = "300px 0 0 0";
+          borderPos = "100% 100%";
+        } else if (previousDirection === "down") {
+          borderRadius = "0 0 0 300px";
+          borderPos = "100% 0";
+        } else {
+          angle = "0deg";
+        }
+        direction = "right";
+      } else if (dx === 0 && dy === -1) {
+        if (previousDirection === "up") {
+          borderRadius = "0 300px 0 0";
+          borderPos = "0 100%";
+        } else if (previousDirection === "down") {
+          borderRadius = "0 0 300px 0";
+          borderPos = "0 0";
+        } else {
+          angle = "0deg";
+        }
+        direction = "left";
+      } else {
+        console.log(
+          `Square (${x}, ${y}) is not adjacent to the previous square.`
+        );
+        return;
+      }
+
+      // style previous tiles
+      const previousTileStyle = borderRadius
+        ? `border-radius: ${borderRadius}; background: repeating-radial-gradient(ellipse farthest-corner at ${borderPos}, #dbd1b4 0 5%, #c2b280 5% 10%);`
+        : `background: repeating-linear-gradient(${angle},#dbd1b4 0 7%,#c2b280 7% 14%)`;
+      // const myClass = `.color-${currentColor} { border-radius: 0 0 0 300px; background: repeating-radial-gradient(ellipse farthest-corner at 100% 0, #dbd1b4 0 5%, #c2b280 5% 10%); }`; // define your CSS class as a string
+      const previousTile = `.color-${currentColor - 1} { ${previousTileStyle}}`;
+      styleSheet.insertRule(previousTile); // insert the CSS class into the stylesheet
+
+      setPreviousDirection(direction);
+      // console.log(
+      //   `Square (${x}, ${y}) is filled by moving ${direction} from square (${prevX}, ${prevY}).`
+      // );
+      const newGrid = grid.map((row, i) =>
+        row.map((col, j) => (i === x && j === y ? currentColor : col))
+      );
+      setGrid(newGrid);
+      setCurrentColor(currentColor + 1);
+      setPrevX(x);
+      setPrevY(y);
+    }
+  };
+
+  //
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="grid-container">
+        <div className="grid-wrapper">
+          <p>Number of squares unfilled: {numUnfilledSquares}</p>
+          {grid.map((row, x) => (
+            <div key={x} className="grid-row">
+              {row.map((col, y) => (
+                <div
+                  key={`${x}-${y}`}
+                  className={`grid-square color-${col}`}
+                  onMouseEnter={() => handleSquareClick(x, y)}
+                />
+              ))}
+            </div>
+          ))}
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Grid;
