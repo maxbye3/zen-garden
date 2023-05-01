c891b60dc8fe4389db2af6d7257115c2909a328b
max bye <mbye@dare.global>
2023-05-01 16:58:05 +0100

    buiuld files

---
+++ _next/static/REdcAqZ-qNIiffn1tCSRF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7d0517826a0c4ac5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
